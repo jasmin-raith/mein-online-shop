d66438641c64f7c73ceb00b9c6fd2000d787e1eb
Jasmin Raith <jasminraith@icloud.com>
2024-04-19 15:33:29 +0200

    component product added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDom from "react-dom"
 import Navbar from "./components/navbar";
+import Product from "./components/product";
 import "bootstrap/dist/css/bootstrap.min.css"
 
-ReactDom.render(<Navbar/>, document.getElementById("root"));
+ReactDom.render(<Product/>, document.getElementById("root"));
