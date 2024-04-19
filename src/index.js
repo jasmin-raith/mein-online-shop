67842b45d9d42376adf00eaaae2d2f5bde0860ad
Jasmin Raith <jasminraith@icloud.com>
2024-04-19 14:53:36 +0200

    component navbar created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDom from "react-dom"
+import Navbar from "./components/navbar";
 
-const element = <h1>Hello World</h1>;
-
-ReactDom.render(element, document.getElementById("root"));
+ReactDom.render(<Navbar/>, document.getElementById("root"));
