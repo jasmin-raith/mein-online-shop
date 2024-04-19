d9cf98b68ce184912667faa5217a34630cc1af16
Jasmin Raith <jasminraith@icloud.com>
2024-04-19 15:06:29 +0200

    bootsstrap installed and navbar added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     state = {  } 
     render() { 
-        return <nav>Navigation</nav>
+        return <nav className="navbar bg-light">
+                    <div className="container-fluid">
+                      <a className="navbar-brand" href="/">Navbar</a>
+                    </div>
+               </nav>
     }
 }
  
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDom from "react-dom"
 import Navbar from "./components/navbar";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDom.render(<Navbar/>, document.getElementById("root"));
