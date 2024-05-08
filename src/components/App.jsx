89d607e92602223b785cb996541373912580e883
Jasmin Raith <jasminraith@icloud.com>
2024-05-08 14:55:59 +0200

    shopping cart added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import Navbar from "./navbar";
 import Product from "./product";
+import ShoppingCart from './shopping-cart';
 
 class App extends Component {
     state = {  } 
     render() { 
         return  <React.Fragment>
-                    <Navbar/>
-                    <div className='product-container'>
-                        <Product image="tomatoes.jpg" title="Tomaten" description="Frische, geschmacksitensive Strauchtomaten aus Kampanien von Süditalien."/>
-                        <Product image="cucumbers.jpg" title="Gurken" description="Knackige Gurken aus der Provinz Almería von Südspanien."/>
-                        <Product image="apples.jpg" title="Äpfel" description="Beste Pink Lady Äpfel aus Chile. Sie sind sehr süß, saftig und aromatisch im Geschmack."/>
-                        <Product image="pears.jpg" title="Birnen" description="Saftig, süße Birnen aus den Niederlanden."/>
+                    <Navbar/> 
+                    <div className='main-container'>
+                        <div className='product-container'>
+                            <Product image="tomatoes.jpg" title="Tomaten" description="Frische, geschmacksitensive Strauchtomaten aus Kampanien von Süditalien."/>
+                            <Product image="cucumbers.jpg" title="Gurken" description="Knackige Gurken aus der Provinz Almería von Südspanien."/>
+                            <Product image="apples.jpg" title="Äpfel" description="Beste Pink Lady Äpfel aus Chile. Sie sind sehr süß, saftig und aromatisch im Geschmack."/>
+                            <Product image="pears.jpg" title="Birnen" description="Saftig, süße Birnen aus den Niederlanden."/>
+                        </div>
+                        <ShoppingCart/>
                     </div>
                 </React.Fragment>
     }
---
+++ src/components/shopping-cart.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class ShoppingCart extends Component {
+    state = {  } 
+    render() { 
+        return <div className="shopping-cart">
+            <h2>Warenkorb</h2>
+        </div>;
+    }
+}
+ 
+export default ShoppingCart;
