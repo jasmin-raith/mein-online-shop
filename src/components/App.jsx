247934b7d580183c3d0672c33eef2079ce9e2db0
Jasmin Raith <jasminraith@icloud.com>
2024-05-22 13:28:39 +0200

    description changed for apples

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,7 +33,7 @@ class App extends Component {
                         <div className='product-container'>
                             <Product onAdd={() => this.addItem(1, 'Tomaten', 2.90)} image="tomatoes.jpg" title="Tomaten" description="Frische, geschmacksitensive Strauchtomaten aus Kampanien von Süditalien."/>
                             <Product onAdd={() => this.addItem(1, 'Gurken', 1.90)} image="cucumbers.jpg" title="Gurken" description="Knackige Gurken aus der Provinz Almería von Südspanien."/>
-                            <Product onAdd={() => this.addItem(1, 'Äpfel', 4.90)} image="apples.jpg" title="Äpfel" description="Beste Pink Lady Äpfel aus Chile. Sie sind sehr süß, saftig und aromatisch im Geschmack."/>
+                            <Product onAdd={() => this.addItem(1, 'Äpfel', 4.90)} image="apples.jpg" title="Äpfel" description="Pink Lady Äpfel aus Chile. Sie sind sehr süß, saftig und aromatisch im Geschmack."/>
                             <Product onAdd={() => this.addItem(1, 'Birnen', 3.90)} image="pears.jpg" title="Birnen" description="Saftig, süße Birnen aus den Niederlanden."/>
                         </div>
                         <ShoppingCart items={this.state.items}/>
